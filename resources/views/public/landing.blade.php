ba3343cf9fd40d187de950478ff5226f7472f574
Danil Hendra Suryawan <danilhendrasuryawan@gmail.com>
2020-02-21 22:39:12 +0700

    Landing page beres!!

--- resources/views/public/landing.blade.php
+++ resources/views/public/landing.blade.php
@@ -4,7 +4,7 @@
     <section id="pengumuman" class="">
       <div class="container">
         <div id="pengumuman-header" class="row">
-          <h2 class="col-sm-12 col-md-12 col-lg-9">
+          <h2 class="section-title col-sm-12 col-md-12 col-lg-9">
             <strong>Pengumuman</strong>
           </h2>
           <nav aria-label="Pengumuman navigation" id="nav-pagination" class="col-sm-5 col-lg-3">
@@ -75,26 +75,29 @@
       </div>
     </section>
 
-    {{-- <section id="events" class="container-fluid">
+    <section id="events" class="container-fluid">
       <div id="events-content" class="container">
-        <h2>Events</h2>
+        <h2 class="section-title">Events</h2>
         <div class="event-item row">
           <div class="event-date col-sm-2">
             <span class="date">20</span>
             <span class="month">Juni</span>
           </div>
 
-          <div class="event-preview col-sm-6">
+          <div class="event-preview col-sm-10 col-md-6 mb-3 mb-md-0">
             <h3>Seminar Nasional "Di Balik Tabir Pendidikan"</h3>
             <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
             <a href="">Read more</a>
           </div>
 
-          <div class="event-img col-sm-3">
+          <div class="event-img col-sm-4 d-none d-md-flex">
             <a href="">
-              <img src="{{ asset('images/poster1.jpg') }}" alt="">
+              {{-- <img src="{{ asset('images/poster1.jpg') }}" alt=""> --}}
               <div class="hover-preview">
-
+                <div class="hover-content">
+                  <span>Seminar Nasional</span>
+                  <span>Oleh Fakultas Teknologi</span>
+                </div>
               </div>
             </a>
           </div>
@@ -106,17 +109,20 @@
             <span class="month">Juni</span>
           </div>
 
-          <div class="event-preview col-sm-6">
+          <div class="event-preview col-sm-10 col-md-6">
             <h3>Seminar Nasional Saham</h3>
             <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
             <a href="">Read more</a>
           </div>
 
-          <div class="event-img col-sm-3">
+          <div class="event-img col-sm-4 d-none d-md-flex">
             <a href="">
-              <img src="{{ asset('images/poster2.jpg') }}" alt="">
+              {{-- <img src="{{ asset('images/poster2.jpg') }}" alt=""> --}}
               <div class="hover-preview">
-
+                <div class="hover-content">
+                  <span>Seminar Saham</span>
+                  <span>Oleh Fakultas Teknologi</span>
+                </div>
               </div>
             </a>
           </div>
@@ -128,69 +134,88 @@
             <span class="month">Juni</span>
           </div>
 
-          <div class="event-preview col-sm-6">
+          <div class="event-preview col-sm-10 col-md-6">
             <h3>Turnamen Tenis Lapangan</h3>
             <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
             <a href="">Read more</a>
           </div>
 
-          <div class="event-img col-sm-3">
+          <div class="event-img col-sm-4 d-none d-md-flex">
             <a href="">
-              <img src="{{ asset('images/poster3.jpg') }}" alt="">
+              {{-- <img src="{{ asset('images/poster3.jpg') }}" alt=""> --}}
               <div class="hover-preview">
-
+                <div class="hover-content">
+                  <span>Turnamen Tennis</span>
+                  <span>Oleh Fakultas Teknologi</span>
+                </div>
               </div>
             </a>
           </div>
         </div>
       </div>
-    </section> --}}
+    </section>
 
     <section id="berita" class="container-fluid">
       <div id="berita-content" class="container">
-        <h2><strong>Berita Terbaru</strong></h2>
+        <h2 class="section-title"><strong>Berita Terbaru</strong></h2>
         <div class="row no-gutters row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-4">
           <div class="berita-item col p-1 p-md-2">
-            <div class="berita-item-content">
-              
+            <div class="berita-item-content pt-2 pl-2">
+              <div class="berita-tag">Health</div>
+              <div class="berita-date">30 Agustus 2020</div>
+              <a href="#" class="berita-title">Lorem ipsum dolor sit amet consectetur</a>
             </div>
           </div>
           <div class="berita-item col p-1 p-md-2">
-            <div class="berita-item-content">
-
+            <div class="berita-item-content pt-2 pl-2">
+              <div class="berita-tag">Health</div>
+              <div class="berita-date">30 Agustus 2020</div>
+              <a href="#" class="berita-title">Lorem ipsum dolor sit amet consectetur</a>
             </div>
           </div>
           <div class="berita-item col p-1 p-md-2">
-            <div class="berita-item-content">
-
+            <div class="berita-item-content pt-2 pl-2">
+              <div class="berita-tag">Health</div>
+              <div class="berita-date">30 Agustus 2020</div>
+              <a href="#" class="berita-title">Lorem ipsum dolor sit amet consectetur</a>
             </div>
           </div>
           <div class="berita-item col p-1 p-md-2">
-            <div class="berita-item-content">
-
+            <div class="berita-item-content pt-2 pl-2">
+              <div class="berita-tag">Health</div>
+              <div class="berita-date">30 Agustus 2020</div>
+              <a href="#" class="berita-title">Lorem ipsum dolor sit amet consectetur</a>
             </div>
           </div>
         </div>
 
-        <div class="row no-gutters row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-4 d-none d-lg-flex">
+        <div class="row no-gutters row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-4">
           <div class="berita-item col p-1 p-md-2">
-            <div class="berita-item-content">
-              
+            <div class="berita-item-content pt-2 pl-2">
+              <div class="berita-tag">Health</div>
+              <div class="berita-date">30 Agustus 2020</div>
+              <a href="#" class="berita-title">Lorem ipsum dolor sit amet consectetur</a>
             </div>
           </div>
           <div class="berita-item col p-1 p-md-2">
-            <div class="berita-item-content">
-
+            <div class="berita-item-content pt-2 pl-2">
+              <div class="berita-tag">Health</div>
+              <div class="berita-date">30 Agustus 2020</div>
+              <a href="#" class="berita-title">Lorem ipsum dolor sit amet consectetur</a>
             </div>
           </div>
           <div class="berita-item col p-1 p-md-2">
-            <div class="berita-item-content">
-
+            <div class="berita-item-content pt-2 pl-2">
+              <div class="berita-tag">Health</div>
+              <div class="berita-date">30 Agustus 2020</div>
+              <a href="#" class="berita-title">Lorem ipsum dolor sit amet consectetur</a>
             </div>
           </div>
           <div class="berita-item col p-1 p-md-2">
-            <div class="berita-item-content">
-
+            <div class="berita-item-content pt-2 pl-2">
+              <div class="berita-tag">Health</div>
+              <div class="berita-date">30 Agustus 2020</div>
+              <a href="#" class="berita-title">Lorem ipsum dolor sit amet consectetur</a>
             </div>
           </div>
         </div>
