f309fdf51a2799879aa2f06c0542bc866c57dd50
Danil Hendra <danilhendrasuryawan@gmail.com>
2020-06-01 12:30:54 +0700

    Tampung data page 2 di kontroller

--- app/Http/Controllers/FormAlumniController.php
+++ app/Http/Controllers/FormAlumniController.php
@@ -7,25 +7,36 @@ use Illuminate\Http\Request;
 class FormAlumniController extends Controller
 {
   function post(Request $request) {
-    $data = array(
+    $data = [
       // Assign form data dari page 1
-      "namaLengkap" => $request->input('namaLengkap'),
-      "npm" => $this->toString($request->input('npm')),
-      "email" => $request->input('email'),
-      "noTelp" => $request->input('noTelp'),
-      "jenisKelamin" => $request->input('jenisKelamin'),
-      "tempatLahir" => $request->input('tempatLahir'),
-      "tglLahir" => $request->input('tglLahir'),
-      "alamatTinggal" => $request->input('alamatTinggal'),
-      "programStudi" => $request->input('programStudi'),
-      "bulanYudisium" => $request->input('bulanYudisium'),
-      "bulanWisuda" => $request->input('bulanWisuda'),
+      // "namaLengkap" => $request->input('namaLengkap'),
+      // "npm" => $this->toString($request->input('npm')),
+      // "email" => $request->input('email'),
+      // "noTelp" => $request->input('noTelp'),
+      // "jenisKelamin" => $request->input('jenisKelamin'),
+      // "tempatLahir" => $request->input('tempatLahir'),
+      // "tglLahir" => $request->input('tglLahir'),
+      // "alamatTinggal" => $request->input('alamatTinggal'),
+      // "programStudi" => $request->input('programStudi'),
+      // "bulanYudisium" => $request->input('bulanYudisium'),
+      // "bulanWisuda" => $request->input('bulanWisuda'),
 
       // Assign form data dari page 2
-
+      "sudahKerja" => $request->input('sudahKerja'),
+      "bulanTerimaKerja" => $this->if_exists($request->input('bulanTerimaKerja')),
+      "gajiPertama" => $this->if_exists($request->input('gajiPertama')),
+      "namaPerusahaan" => $this->if_exists($request->input('namaPerusahaan')),
+      "jabatan" => $this->if_exists($request->input('jabatan')),
+      "keteranganJabatan" => $this->if_exists($request->input('ketaranganJabatan')),
+      "alamatPerusahaan" => $this->if_exists($request->input('alamatPerusahaan')),
+      "emailPerusahaan" => $this->if_exists($request->input('emailPerusahaan')),
+      "namaAtasan" => $this->if_exists($request->input('namaAtasan')),
+      "jabatanAtasan" => $this->if_exists($request->input('jabatanAtasan')),
+      "jenisPerusahaan" => $this->if_exists($request->input('jenisPerusahaan')),
+      "ukuranTempatKerja" => $this->if_exists($request->input('ukuranTempatKerja')),
 
       // Assign form data dari page 3
-    );
+    ];
 
     return (
       response($data, 200)
@@ -35,5 +46,13 @@ class FormAlumniController extends Controller
 
   private function toString($input) {
     return strval($input);
+  }
+
+  private function if_exists($input) {
+    return (
+      $input 
+      && $input != "" 
+      && $input != null
+    ) ? $input : "-";
   }
 }
--- resources/views/public/templates/pubHeader.blade.php
+++ resources/views/public/templates/pubHeader.blade.php
@@ -28,8 +28,6 @@
 
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
 
-    {{-- <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css"> --}}
-
   </head>
 
 
