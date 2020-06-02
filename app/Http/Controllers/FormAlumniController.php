384519f29b9bb2486c3df02225b5f1e7b1de73b0
Danil Hendra <danilhendrasuryawan@gmail.com>
2020-06-02 09:13:35 +0700

    Form survey alumni selesai

--- app/Http/Controllers/FormAlumniController.php
+++ app/Http/Controllers/FormAlumniController.php
@@ -9,17 +9,17 @@ class FormAlumniController extends Controller
   function post(Request $request) {
     $data = [
       // Assign form data dari page 1
-      // "namaLengkap" => $request->input('namaLengkap'),
-      // "npm" => $this->toString($request->input('npm')),
-      // "email" => $request->input('email'),
-      // "noTelp" => $request->input('noTelp'),
-      // "jenisKelamin" => $request->input('jenisKelamin'),
-      // "tempatLahir" => $request->input('tempatLahir'),
-      // "tglLahir" => $request->input('tglLahir'),
-      // "alamatTinggal" => $request->input('alamatTinggal'),
-      // "programStudi" => $request->input('programStudi'),
-      // "bulanYudisium" => $request->input('bulanYudisium'),
-      // "bulanWisuda" => $request->input('bulanWisuda'),
+      "namaLengkap" => $request->input('namaLengkap'),
+      "npm" => $this->toString($request->input('npm')),
+      "email" => $request->input('email'),
+      "noTelp" => $request->input('noTelp'),
+      "jenisKelamin" => $request->input('jenisKelamin'),
+      "tempatLahir" => $request->input('tempatLahir'),
+      "tglLahir" => $request->input('tglLahir'),
+      "alamatTinggal" => $request->input('alamatTinggal'),
+      "programStudi" => $request->input('programStudi'),
+      "bulanYudisium" => $request->input('bulanYudisium'),
+      "bulanWisuda" => $request->input('bulanWisuda'),
 
       // Assign form data dari page 2
       "sudahKerja" => $request->input('sudahKerja'),
@@ -36,6 +36,31 @@ class FormAlumniController extends Controller
       "ukuranTempatKerja" => $this->if_exists($request->input('ukuranTempatKerja')),
 
       // Assign form data dari page 3
+      "besarMetodePerkuliahan" => $request->input('besarMetodePerkuliahan'),
+      "besarMetodeDemonstrasi" => $request->input('besarMetodeDemonstrasi'),
+      "besarMetodeProyekRiset" => $request->input('besarMetodeProyekRiset'),
+      "besarMetodeMagang" => $request->input('besarMetodeMagang'),
+      "besarMetodePraktikum" => $request->input('besarMetodePraktikum'),
+      "besarMetodeKerjaLapangan" => $request->input('besarMetodeKerjaLapangan'),
+      "besarMetodeDiskusi" => $request->input('besarMetodeDiskusi'),
+      "kapanCariKerja" => $request->input('kapanCariKerja'),
+      "metodeCariKerja" => $this->if_exists($request->input('metodeCariKerja')),
+      "kapanDapatKerja" => $this->if_exists($request->input('kapanDapatKerja')),
+      "jumlahPerusahaanSudahDilamar" => $this->if_exists($request->input('jumlahPerusahaanSudahDilamar')),
+      "jumlahResponLamaran" => $this->if_exists($request->input('jumlahResponLamaran')),
+      "jumlahUndanganWawancara" => $this->if_exists($request->input('jumlahUndanganWawancara')),
+      "lanjutKuliah" => $request->input('lanjutKuliah'),
+      "lokasiLanjutKuliah" => $this->if_exists($request->input('lokasiLanjutKuliah')),
+      "gambaranSituasi" => $request->input('gambaranSituasi'),
+      "aktifCariKerja" => $this->if_exists($request->input('aktifCariKerja')),
+      "bidangKerja" => $this->if_exists($request->input('bidangKerja')),
+      "sumberDanaKuliah" => $request->input('sumberDanaKuliah'),
+      "pendapatanUtama" => $this->if_exists($request->input('pendapatanUtama')),
+      "pendapatanLembur" => $this->if_exists($request->input('pendapatanLembur')),
+      "pendapatanLain" => $this->if_exists($request->input('pendapatanLain')),
+      "hubunganStudiKerja" => $this->if_exists($request->input('hubunganStudiKerja')),
+      "pendidikanSesuaiPekerjaan" => $this->if_exists($request->input('pendidikanSesuaiPekerjaan')),
+
     ];
 
     return (
